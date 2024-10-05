6bd7fb542a8b8275d62958ff73ca7b64f4d7485b
gleisonbt <gleisoneinstein@hotmail.com>
2024-10-04 21:26:03 -0300

    add list dynamic

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var alunos = [];
+var qdteElementos = 0;
 
 function guardarDados(event){
     var nome = document.getElementById("nome").value;
@@ -16,6 +17,7 @@ function guardarDados(event){
     event.preventDefault();
 
     limparCampos();
+    exibirAlunos();
 }
 
 function limparCampos(){
@@ -23,4 +25,15 @@ function limparCampos(){
     document.getElementById("idade").value = "";
     document.getElementById("linguagem").
         selectedIndex = 0;
+}
+
+function exibirAlunos(){
+    
+    var linha = alunos[qdteElementos].nome + " - " + alunos[qdteElementos].idade + " - " + alunos[qdteElementos].linguagem;
+    
+    var item = document.createElement("li");
+    item.innerHTML = linha;
+    document.getElementById("listaAlunos").appendChild(item);
+
+    qdteElementos++;
 }
